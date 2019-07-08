9ce5beb50014c185b2d9f1c243e66d0246f7a0d7
eonil <pub.dev@eonil.com>
2019-07-08 19:07:58 +0900

    Revive multi-product config.

--- Package.swift
+++ Package.swift
@@ -10,13 +10,13 @@ let package = Package(
     products: [
         /// Xcode 11 Beta has troubles with multiple product package...
         .library(name: "TreeV3", type: .static, targets: ["TreeV3"]),
-//        .library(name: "TreeV3", type: .static, targets: ["TreeV3Core", "TreeV3Util"]),
-//        .library(name: "PDTreeV3", type: .static, targets: ["TreeV3Core", "TreeV3Util", "PDTreeV3"]),
+        .library(name: "PDTreeV3", type: .static, targets: ["PDTreeV3"]),
     ],
     dependencies: [
         .package(url: "https://github.com/eonil/BTree", .branch("master")),
     ],
     targets: [
+        /// A target is required to build a **module**.
         .target(
             name: "TreeV3",
             dependencies: [],
